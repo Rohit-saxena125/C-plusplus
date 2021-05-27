f3bee168260a7f216d5820cd63c6c727acd06178
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-27 16:43:49 +0530

    Initial commit

---
+++ Loops/Pattern3.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+
+int main()
+{
+    int n, m;
+    int lines;
+    printf("Enter the numberof lines: ");
+    scanf("%d", &lines);
+    for(n=1; n<=lines; n++)
+    {
+        for(m=1; m<=n; m++)
+            printf("%d ", m);
+        printf("\n");    
+    }
+     for(n=lines; n>0; n--)
+    {
+        for(m=1; m<=n; m++)
+            printf("%d ", m);
+        printf("\n");    
+    }
+    return 0;
+}
