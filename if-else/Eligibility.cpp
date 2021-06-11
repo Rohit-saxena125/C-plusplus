8d4673111823f3bc4e876406db36eb8579f5d711
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-06-11 15:41:56 +0530

    Initial commit

---
+++ if-else/Eligibility.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+    int age;
+
+    cin>>age;
+
+    if (age>=18)
+    {
+        printf("You are eligible");
+    }
+    else
+    {
+        printf("You are not eligible");
+    }
+}
